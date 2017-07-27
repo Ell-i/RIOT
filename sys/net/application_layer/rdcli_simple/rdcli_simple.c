10443ddf0aba4f1122b4bcfe4a872d583f3ce3b2
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-07-27 11:37:06 +0200

    fixup! rdcli_simple: moved qstring creation to common module

--- sys/net/application_layer/rdcli_simple/rdcli_simple.c
+++ sys/net/application_layer/rdcli_simple/rdcli_simple.c
@@ -44,29 +44,19 @@ static const sock_udp_ep_t remote = {
 
 int rdcli_simple_register(void)
 {
-    char lt[11];
-
     /* build the initial CON packet */
     int res = gcoap_req_init(&pkt, buf, sizeof(buf), COAP_METHOD_POST,
                              "/.well-known/core");
     if (res < 0) {
         return res;
     }
+    /* make packet confirmable */
     coap_hdr_set_type(pkt.hdr, COAP_TYPE_CON);
-
-
-    /* extend the url with some query string options */
-    res = gcoap_add_qstring(&pkt, "ep", rdcli_common_get_ep());
+    /* add Uri-Query options */
+    res = rdcli_common_add_qstring(&pkt);
     if (res < 0) {
         return res;
     }
-    size_t pos = fmt_u32_dec(lt, RDCLI_LT);
-    lt[pos] = '\0';
-    res = gcoap_add_qstring(&pkt, "lt", lt);
-    if (res < 0) {
-        return res;
-    }
-
     /* finish, we don't have any payload */
     ssize_t len = gcoap_finish(&pkt, 0, COAP_FORMAT_TEXT);
     return (int)gcoap_req_send2(buf, len, &remote, NULL);
